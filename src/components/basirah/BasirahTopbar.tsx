e3f6a665f8c278244d3b16acd6eec21d1e5b89ea
Ali Mobarki <ali2030it@gmail.com>
2026-09-21 23:24:24 +0300

    Reduce duplicate topbar authentication request

--- src/components/basirah/BasirahTopbar.tsx
+++ src/components/basirah/BasirahTopbar.tsx
@@ -20,8 +20,10 @@ export function BasirahTopbar() {
 
     async function loadProfile() {
       const {
-        data: { user },
-      } = await supabase.auth.getUser();
+        data: { session },
+      } = await supabase.auth.getSession();
+
+      const user = session?.user;
 
       if (!user) return;
 
